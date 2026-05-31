fd402123c1c7619ec27bfff699ece1566e97f815
Marcos <marcosvieira.vs11@gmail.com>
2026-05-31 11:33:22 -0400

    05 - Antecessor e Sucessor (2)

--- exercício-05.py
+++ exercício-05.py
@@ -1,7 +1,16 @@
-try: 
-    print(f'{'='*30} \n   Antecessor e Sucessor \n{'='*30}')
-    na = int(input('Digite um número: '))
-    print(f'O antecessor de {na} é {na - 1} e o sucessor é {na + 1} ')
-
-except ValueError:
-    print('ERRO! Este programa aceita apenas números inteiros. Tente novamente!')
+cont = 's'
+while(cont.lower() != 'n'):
+    try:    
+        tit = 'Antecessor e Sucessor'.center(30, ' ')
+        sep = '='*30
+        print(sep)
+        print(tit)
+        print(sep)
+        na = int(input('Digite um número: '))
+        print(f'O antecessor de {na} é {na - 1} e o sucessor é {na + 1} ')
+        cont = input('Quer continuar com outro número? (s/n): ')
+        if cont.lower() != 's':
+            print('Saindo...')
+            break
+    except ValueError:
+        print('ERRO! Este programa aceita apenas números inteiros. Tente novamente!')

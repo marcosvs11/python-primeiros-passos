f8242061cf60abf8569b743e35f1d4048f4884d2
Marcos <marcosvieira.vs11@gmail.com>
2026-06-17 20:19:35 -0400

    Módulo Prática 02

--- exercício-16.py
+++ exercício-16.py
@@ -1,8 +1,11 @@
 import random
-enter = ''
-while enter != 'n':
-    enter = input('Digite "ENTER" para continuar ou "N" para parar: ' )
-    if enter == 'n':
+resp = 's'
+while resp != 'n':
+    nomes = ['Marcos', 'Giovanni', 'Annelise', 'Aline', 'João Victor', 'Alan', 'Laryssa']
+    print('Sorteador de Nomes'. center(30, ' '))
+    enter  = input('Digite "ENTER" para começar: ')
+    ale = random.choice(nomes)
+    print(f'Você sorteou: {ale}')
+    resp = input('Quer sortear o próximo? (s/n): ')
+    if resp != 's':
         break
-    print(f'Você tirou: {random.randint(1,6)}')
-

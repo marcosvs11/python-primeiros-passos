877ef9713381768f7a9ba43ee5e88e08b6e818e0
Marcos <marcosvieira.vs11@gmail.com>
2026-06-27 09:36:31 -0400

    25 - 1° Nome e o Último

---
+++ exercício-25.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome completo: ')
+prim = nome.title().split()[0]
+ult = nome.title().split()[-1]
+print(f'O seu primeiro nome é: {prim}.')
+print(f'E o seu último nome é: {ult}')

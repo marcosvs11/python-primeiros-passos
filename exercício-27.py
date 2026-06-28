32a7f3e1158c23a2e5de3529833d0178d6901352
Marcos <marcosvieira.vs11@gmail.com>
2026-06-28 13:13:37 -0400

    27 - Jogo de Adivinhação (random.randint)

--- exercício-27.py
+++ exercício-27.py
@@ -1,3 +1,4 @@
+from random import randint
 resp = 's'
 while resp != 'n':
     sep = 30*'='
@@ -5,13 +6,15 @@ while resp != 'n':
     print(sep)
     print(tit)
     print(sep)
-    esco = 3
+    print('DICA: Entre 0 e 5.')
+    esco = randint(0, 5)
     tent = int(input('Digite o número: '))
     if tent == esco:
         print('Parabéns, você acertou o número!')
         break
     else:
-        print('Vish...Parece que você digitou o número incorreto!')
+        print('Vish...Parece que você adivinhou o número incorreto!')
+        print(f'O número que estava pensando era o {esco}.')
         resp = input(('Quer tentar novamente? (s/n): ')).strip()
         if resp.lower() != 's':
             print('Ah, que pena...Achei que você iria conseguir acertar!')

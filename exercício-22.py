d884020f669bee908aca285777d9687f3f396c39
Marcos <marcosvieira.vs11@gmail.com>
2026-06-27 22:35:31 -0400

    22 - Mostrar Ordem Númerica

--- exercício-22.py
+++ exercício-22.py
@@ -1,4 +1,9 @@
-n1 = input('Digite um número: ')
-# mais tarde eu tento
-
-
+n1 = int(input('Digite um número: '))
+n1 = str(n1)
+print(f'Unidade: {n1[-1]}')
+if len(n1) >= 2:
+    print(f'Dezena: {n1[-2]}')
+if len(n1) >= 3:
+    print(f'Centena: {n1[-3]}')
+if len(n1) == 4:
+    print(f'Milhar: {n1[-4]}')

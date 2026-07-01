45465840d8fec1e107b6db250f8ce73d498dfbd9
Marcos <marcosvieira.vs11@gmail.com>
2026-07-01 19:49:58 -0400

    32 - Valor Maior e Menor (funções)

--- exercício-32.py
+++ exercício-32.py
@@ -9,16 +9,8 @@ while resp != 'n':
         n1 = int(input('Digite o 1° número: '))
         n2 = int(input('Digite o 2° número: '))
         n3 = int(input('Digite o 3° número: '))
-        menor = n1
-        if n2 < n1 and n2 < n3:
-            menor = n2
-        if n3 < n1 and n3 < n2:
-            menor = n3
-        maior = n1
-        if n2 > n1 and n2 > n3:
-            maior = n2
-        if n3 > n1 and n3 > n2:
-            maior = n3
+        menor = min(n1, n2, n3)
+        maior = max(n1, n2, n3)
         print(f'O menor número digitado é: {menor}.')
         print(f'E o maior digitado é: {maior}.')
         resp = input('Quer tentar novamente com outros números? (s/n): ').strip()

86d77744cc24674e2c927d409eec252a7989a8b7
Marcos <marcosvieira.vs11@gmail.com>
2026-07-12 19:36:07 -0400

    55 - Maior e Menor Pesos

--- exercício-55.py
+++ exercício-55.py
@@ -1,14 +1,13 @@
+# referência para que a 1° tentativa aconteça.
 peso_maior = 0
-peso_menor = 0
-troca = 0
+# mesma situação, mas nesse caso precisa de um positivo infinito.
+peso_menor = float('inf')
 for i in range(5):
     peso = float(input(f'Digite a peso da {i + 1}° pessoa: '))
     if peso > peso_maior:
         peso_maior = peso
-    elif peso < peso_menor:
+# dois if independentes para não ter interferência.
+    if peso < peso_menor:
         peso_menor = peso
-    else: 
-        peso_menor = peso
-
 print(f'MAIOR: {peso_maior:.2f}')
 print(f'MENOR: {peso_menor:.2f}')

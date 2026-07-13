974abb9166459239d437a39a3bc293e81cd9b41e
Marcos <marcosvieira.vs11@gmail.com>
2026-07-12 20:11:45 -0400

    53 - Verificador de Palíndromo

--- exercício-53.py
+++ exercício-53.py
@@ -1,7 +1,17 @@
 # Palíndromo é quando uma frase pode ser lida exatamente do mesmo jeito, seja da esquerda para direita quanto da direita para esquerda
-frase = input('Digite uma frase qualquer: ').strip().lower().replace(' ', '')
-f_invertida = frase.rfind(frase)
-if frase == f_invertida:
-    print('PALÍDROMO')
+sep = 30 * '='
+tit = 'Verificador de Palíndromo'.center(30, ' ')
+print(sep)
+print(tit)
+print(sep)
+frase = input('Digite uma frase qualquer: ').strip().lower()
+separada = frase.split()
+junto = ''.join(separada)
+inverso = ''
+for i in range(len(junto) - 1, -1, -1):
+    inverso += junto[i]
+if junto == inverso:
+    descisao = 'é palíndromo!'
 else:
-    print('FALSO')
+    descisao = 'não é palíndromo!'
+print(f'A frase {junto} ao contrário é {inverso}. Portanto, {descisao}')

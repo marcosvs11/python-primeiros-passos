fce126804341cd06fa5af6e0c309c4ca8402b68a
Marcos <marcosvieira.vs11@gmail.com>
2026-07-20 12:43:30 -0400

    62 - Calculadora P.A. - Condição + While + Anotações

--- exercício-62.py
+++ exercício-62.py
@@ -7,18 +7,24 @@ try:
     n1 = int(input('Digite o 1°termo: '))
     razao = int(input('Digite o valor da razão: '))
     resultado = n1
-    quant_termos = 10 
+    # 1° loop será os 10 primeiros termos.
+    quant_termos = 10
+    # Condição para repetir até o usuário sair.
     while quant_termos != 0:
         cont = 0
+        # Usuário decide quantos termos mostrar.
         while cont < quant_termos:
             cont += 1
+            # Para tirar a "," do último número.
             if cont == quant_termos:
                 print(resultado, end='')
             else:    
                 print(resultado, end=', ') 
                 resultado += razao
         print(f'\nFIM DOS {quant_termos}° TERMOS!')
+        # Ele decide a quantidade de termos.
         quant_termos = int(input('Quantos termos será revelado agora? (número 0 para sair): '))
+        # Condição para o programa parar.
         if quant_termos == 0:
             print('Saindo...')
             break

cb0240b2acd7783510f42f71f947ec6023852837
Marcos <marcosvieira.vs11@gmail.com>
2026-09-05 22:32:42 -0400

    102 - Função c/ Fatorial

---
+++ exercicios/exercicio-102.py
@@ -0,0 +1,42 @@
+def fatorial(numero, show=False):
+    if numero == 0:
+        fat = 1
+        return fat
+
+    if numero < 0:
+        fat = 'Não existe fatorial de número negativo!'
+        return fat
+
+    parametro = numero
+    fat = 1
+
+    while numero != 0:
+        fat *= numero
+        if show:
+            if numero - 1 == 0:
+                print(f'{numero} = ', end='')
+            else:
+                print(f'{numero} x', end=' ')
+
+        numero -= 1
+
+    return fat
+
+# Programa principal
+mostrar = False
+while True:
+    try:
+        num = int(input('Digite um número: '))
+    except ValueError:
+        print('Entrada inválida, digite apenas números inteiros!')
+        continue
+
+    break
+
+resp = str(input('Quer visualizar as operações? (S/N): ')).strip().upper()
+if resp == 'S':
+    mostrar = True
+
+resultado = fatorial(num, mostrar)
+
+print(f'{num}! = {resultado}')
